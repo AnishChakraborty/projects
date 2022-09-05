19b2efd1fbcf29ccb42da0dce225fd04a666178e
Anish <canishmax@gmail.com>
2022-09-06 03:48:28 +0530

    flight booking using django rest api

---
+++ flightServices/flightApp/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from flightApp.models import Flight,Passenger,Reservation
+import re
+class FlightSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Flight
+        fields = '__all__'
+
+    def validate_flightNumber(self,flightNumber):
+        if(re.match("^[a-zA-Z0-9]*$",flightNumber)==None):
+            raise serializers.ValidationError("invalid flight number")
+        return flightNumber
+
+    def validate(self, data):
+        print('Validate')
+        print(data)
+        print(data['flightNumber'])
+        return data
+class PassengerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Passenger
+        fields = '__all__'
+
+class ReservationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reservation
+        fields = '__all__'
